f0a6f1892bcfdfbdc1c5b0f528f8c485a9c5749a
nikpau <n.paulig@web.de>
2023-06-21 13:35:52 +0200

    imports

--- src/pytsa/__init__.py
+++ src/pytsa/__init__.py
@@ -1,5 +1,6 @@
-from .search_agent import SearchAgent, TimePosition
+from .search_agent import SearchAgent, TimePosition, TargetVessel
 from .structs import LatLonBoundingBox, UTMBoundingBox
+from .targetship import TrajectoryMatcher
 
 __version__ = "2.0.1"
 __author__ = "Niklas Paulig <niklas.paulig@tu-dresden.de>"
