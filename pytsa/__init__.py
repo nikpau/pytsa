6a0c407101fab262f7a97b210d6cc089b70cd691
nikpau <n.paulig@web.de>
2024-02-02 16:30:17 +0100

    add geometry for plotting

--- pytsa/__init__.py
+++ pytsa/__init__.py
@@ -3,6 +3,7 @@ from .structs import BoundingBox, ShipType
 from .tsea.targetship import TrajectoryMatcher
 from .trajectories import Inspector
 from .decode import decode_from_file, decode
+from .visualization import register_plot_dir
 
 
 __version__ = "2.0.1"
