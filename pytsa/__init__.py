4e18d1727beb9c0e7e7edc7b738bcdad40093344
nikpau <n.paulig@web.de>
2024-02-09 16:17:57 +0100

    version

--- pytsa/__init__.py
+++ pytsa/__init__.py
@@ -5,7 +5,14 @@ from .trajectories import Inspector, ExampleRecipe
 from .decode import decode_from_file, decode
 from .visualization import register_plot_dir
 from . import visualization
+import tomli, pathlib
+from . import __path__ as projpath
 
+# Parse version from pyproject.toml
+__ppj = pathlib.Path(projpath[0]).parent / "pyproject.toml"
+with open(__ppj,"rb") as tml:
+    __v = tomli.load(tml)["tool"]["poetry"]["version"]
 
-__version__ = "2.0.1"
+__version__ = __v
 __author__ = "Niklas Paulig <niklas.paulig@tu-dresden.de>"
+print(__version__)
