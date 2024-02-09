e92a2db38f8310ac2ab6449d79c5ffd99d4119fd
nikpau <n.paulig@web.de>
2024-02-09 16:32:05 +0100

    rollback version pull

--- pytsa/__init__.py
+++ pytsa/__init__.py
@@ -5,14 +5,7 @@ from .trajectories import Inspector, ExampleRecipe
 from .decode import decode_from_file, decode
 from .visualization import register_plot_dir
 from . import visualization
-import tomli, pathlib
-from . import __path__ as projpath
 
-# Parse version from pyproject.toml
-__ppj = pathlib.Path(projpath[0]) / "pyproject.toml"
-with open(__ppj,"rb") as tml:
-    __v = tomli.load(tml)["tool"]["poetry"]["version"]
-
-__version__ = __v
+__version__ = "2.2.0"
 __author__ = "Niklas Paulig <niklas.paulig@tu-dresden.de>"
 print(__version__)
