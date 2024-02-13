84306b2fdcbecfaa9818ac9c3cd67e1346ceb65a
nikpau <n.paulig@web.de>
2024-02-13 12:59:51 +0100

    test file

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Global geographic search area.
 # Outside these bounds, no search will be commenced
-frame = pytsa.LatLonBoundingBox(
+frame = pytsa.BoundingBox(
     LATMIN = 51.85,
     LATMAX = 60.49,
     LONMIN = 4.85,
@@ -12,14 +12,15 @@ frame = pytsa.LatLonBoundingBox(
 )
 
 # File containing AIS messages
-dynamic_data = Path("data/dynamic_data.csv")
-static_data = Path("data/static_data.csv")
+# Replace with the path to your own data ---v
+dynamic_data = Path("path/to/dynamic_data.csv")
+static_data = Path("path/to/static_data.csv")
 
 # Instantiate the search agent with the source file 
 # and the search area
 search_agent = pytsa.SearchAgent(
-    msg12318file=dynamic_data,
-    msg5file=static_data,
+    dynamic_paths=dynamic_data,
+    static_paths=static_data,
     frame=frame,
     preprocessor= lambda df: df[df["speed"] > 0]
 )
@@ -27,29 +28,18 @@ search_agent = pytsa.SearchAgent(
 # Provide a position and time for which the search
 # will be carried out
 tpos = pytsa.TimePosition(
-    timestamp="2021-07-01T00:00:00",
+    timestamp="2021-04-01T08:00:00Z",
     lat=54.00,
-    lon=7.6
+    lon=8.26
 )
-search_agent.init(tpos)
 
-r = search_agent.get_all_ships()
-
-def plot_tracks(ships):
-    
-    f, ax = plt.subplots(1,1,figsize=(10,10))
-    for ship in ships.values():
-        for track in ship.tracks:
-            ax.plot([m.lon for m in track], 
-                    [m.lat for m in track], color="black", alpha=0.1)
-    plt.show()
-    
- #plot_tracks(r)
-
-for _ in range(0,500):
+# Track the target vessels for 100 iterations
+# of 3 seconds each and print the observed
+# position, speed and course
+for _ in range(0,100):
     # Search for TargetVessels
-    tpos.timestamp += 1 # 1 second
-    target_ships = search_agent.get_ships(tpos,search_radius=40)
+    tpos.timestamp += 3 # 3 seconds
+    target_ships = search_agent.freeze(tpos,search_radius=40)
 
     # Extract the current position, speed and
     # course for all found target vessels.
