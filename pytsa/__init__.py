1a843ada6e922bef3cccdfd82e56b813cb45e0a2
nikpau <n.paulig@web.de>
2024-05-07 10:48:43 +0200

    version

--- pytsa/__init__.py
+++ pytsa/__init__.py
@@ -16,6 +16,6 @@ from .visualization import register_plot_dir
 from . import visualization
 
 
-__version__ = "2.3.3"
+__version__ = "2.3.4"
 __author__ = "Niklas Paulig <niklas.paulig@tu-dresden.de>"
 logger.info(f"You are using PyTSA version {__version__}")
