cecb7c1659ecd54d59f8bae8ffde922a83621bd3
nikpau <n.paulig@web.de>
2024-07-23 11:06:48 +0200

    2.3.6 bump

--- pytsa/__init__.py
+++ pytsa/__init__.py
@@ -1,12 +1,4 @@
-# Append shared library path to sys.path
-# from . import __path__ as _PYTSA_DIR
 from .logger import logger
-# import sys
-# from pathlib import Path
-# sharedpath = Path(_PYTSA_DIR[0]) / "shared"
-# logger.info(f"Loading shared libraries")
-# sys.path.append(sharedpath.as_posix())
-# del sharedpath, Path, _PYTSA_DIR
 
 from .tsea.search_agent import SearchAgent, TimePosition, TargetShip
 from .structs import BoundingBox, ShipType
@@ -15,6 +7,6 @@ from .decoder import decode_from_file, decode
 from .visualization import register_plot_dir
 from . import visualization
 
-__version__ = "2.3.5"
+__version__ = "2.3.6"
 __author__ = "Niklas Paulig <niklas.paulig@tu-dresden.de>"
 logger.info(f"You are using PyTSA version {__version__}")
