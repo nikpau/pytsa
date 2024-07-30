9fbcc47538c4360a7e3efc39b51c5488ded71f20
nikpau <n.paulig@web.de>
2024-07-30 10:35:54 +0200

    manylinux support

--- pytsa/__init__.py
+++ pytsa/__init__.py
@@ -7,6 +7,6 @@ from .decoder import decode_from_file, decode
 from .visualization import register_plot_dir
 from . import visualization
 
-__version__ = "2.3.6"
+__version__ = "2.3.6a"
 __author__ = "Niklas Paulig <niklas.paulig@tu-dresden.de>"
 logger.info(f"You are using PyTSA version {__version__}")
