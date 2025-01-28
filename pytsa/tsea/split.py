e3231519da21153016d1354933abc977e04411c3
nikapu <n.paulig@web.de>
2025-01-28 09:02:57 +0100

    singleton stats collector

--- pytsa/tsea/split.py
+++ pytsa/tsea/split.py
@@ -43,6 +43,12 @@ class _TREXStatCollector:
     Simple class for collecting statistics 
     about the split points found.
     """
+    _instance = None
+    def __new__(cls,*args,**kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
     def __init__(self) -> None:
         self._n_rejoined_tracks = 0
         self._speed_change = 0
